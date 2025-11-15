c08ed910437884baba9e2be739d188847b99efdd
mres42 <mario.github@protonmail.com>
2025-11-15 18:19:43 -0300

    shuffle array

---
+++ array-method-exercises/6-shuffle-array.js
@@ -0,0 +1,24 @@
+let arr = [1, 2, 3];
+
+function shuffle(arr) {
+    arr.sort(() => Math.random() - 0.5);
+}
+
+
+
+shuffle(arr);
+console.log(arr);
+shuffle(arr);
+console.log(arr);
+shuffle(arr);
+console.log(arr);
+shuffle(arr);
+console.log(arr);
+shuffle(arr);
+console.log(arr);
+shuffle(arr);
+console.log(arr);
+shuffle(arr);
+console.log(arr);
+shuffle(arr);
+console.log(arr);
